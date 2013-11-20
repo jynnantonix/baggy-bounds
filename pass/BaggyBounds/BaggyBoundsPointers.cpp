919d6bc754a764c774f5d1a526a02088ceb52cd2
Chirantan Ekbote <ekbote@mit.edu>
2013-11-20 14:12:48 +0000

    Add all baggy instrumentation except for slow path
    Since baggy bounds checking will change the control flow of the graph, we
    need to use a function pass instead of a bsic block pass.  The function pass
    loops through all the basic blocks in the function and if it finds any pointer
    arithmetic in the block, splits up the block and inserts a baggy bounds
    checking block in the middle.  The only thing left to do is to write the
    slowPath function and include a branch to it at the end of the baggy bounds
    block.

--- pass/BaggyBounds/BaggyBoundsPointers.cpp
+++ pass/BaggyBounds/BaggyBoundsPointers.cpp
@@ -3,37 +3,93 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 namespace {
-  struct BaggyBoundsPointers : public BasicBlockPass {
+  struct BaggyBoundsPointers : public FunctionPass {
     static char ID;
-    BaggyBoundsPointers() : BasicBlockPass(ID) {}
+    BaggyBoundsPointers() : FunctionPass(ID) {}
+    GlobalVariable *sizeTable;
+    Function *slowPath;
 
-    virtual bool runOnBasicBlock(BasicBlock &BB) {
-      for (BasicBlock::iterator i = BB.begin(), e = BB.end(); i != e; ++i) {
-        if (isa<GetElementPtrInst>(*i)) {
-          GetElementPtrInst *inst = &cast<GetElementPtrInst>(*i);
-          IRBuilder<> builder(inst);
-          Value *tablestart = ConstantInt::get(IntegerType::get(BB.getContext(), 32), 0x78000000);
-          Value *base, *baseptr, *tableoffset, *tableaddr, *hack;
-          LoadInst *size;
+    BasicBlock *createBaggyBlock(BasicBlock *orig, GetElementPtrInst *i) {
+      BasicBlock *baggyBlock = BasicBlock::Create(orig->getContext(), "baggy.check",
+                                                  orig->getParent(), orig);
+      IRBuilder<> builder(baggyBlock);
+      Value *tablestart = ConstantInt::get(IntegerType::get(baggyBlock->getContext(), 32), 0x78000000);
+      Value *base, *baseint, *tableoffset, *tableaddr, *hack, *inst;
+      LoadInst *size;
 
-          base = builder.CreateConstInBoundsGEP1_32(inst->getPointerOperand(), 0);
-          baseptr = builder.CreatePtrToInt(base, IntegerType::get(BB.getContext(), 32));
-          tableoffset = builder.CreateAShr(baseptr, 4);
-          hack = builder.CreateIntToPtr(tablestart, Type::getInt8PtrTy(BB.getContext()));
-          tableaddr = builder.CreateInBoundsGEP(hack, tableoffset);
-          size = builder.CreateLoad(tableaddr, "alloc_size");
-       }
+      // Baggy lookup
+      base = builder.CreateConstInBoundsGEP1_32(i->getOperand(0), 0);
+      baseint = builder.CreatePtrToInt(base, IntegerType::get(baggyBlock->getContext(), 32));
+      tableoffset = builder.CreateAShr(baseint, 4);
+      hack = builder.CreateIntToPtr(tablestart, Type::getInt8PtrTy(baggyBlock->getContext()));
+      tableaddr = builder.CreateInBoundsGEP(sizeTable, tableoffset);
+      size = builder.CreateLoad(tableaddr, "alloc.size");
+
+      // insert arithmetic
+      baggyBlock->getInstList().push_back(i);
+
+      // baggy check
+      Value *combine, *instint, *result, *sizeint, *br;
+
+      instint = builder.CreatePtrToInt(i, IntegerType::get(baggyBlock->getContext(), 32));
+      combine = builder.CreateXor(baseint, instint);
+      sizeint = builder.CreateZExtOrBitCast(size, IntegerType::get(baggyBlock->getContext(), 32));
+      result = builder.CreateAShr(combine, sizeint);
+      br = builder.CreateBr(orig);
+
+      return baggyBlock;
+    }
+
+    virtual bool doInitialization(Module &M) {
+      sizeTable = M.getGlobalVariable("baggy_size_table");
+    }
+
+    virtual bool runOnFunction(Function &F) {
+      for (Function::iterator bb = F.begin(), bbend = F.end(); bb != bbend; ++bb) {
+        BasicBlock *block = &(*bb);
+        for (BasicBlock::iterator i = block->begin(), e = block->end(); i != e; ++i) {
+          if (isa<GetElementPtrInst>(*i)) {
+            BasicBlock *after = block->splitBasicBlock(i, "baggy.split");
+            BasicBlock *baggy;
+
+            // Remove the getelementptrinst from the old block
+            i->removeFromParent();
+
+            // Create the instrumentation block
+            baggy = createBaggyBlock(after, &cast<GetElementPtrInst>(*i));
+
+            // Have control flow through the instrumentation block
+            TerminatorInst *term = block->getTerminator();
+            if (term == NULL) {
+              block->getInstList().push_back(BranchInst::Create(baggy));
+            } else {
+              term->setSuccessor(0, baggy);
+            }
+
+            // Skip the newly created instrumentation basicblock
+            ++bb;
+
+            // We're done with this block
+            break;
+          }
+        }
       }
 
-      return false;
+      return true;
+    }
+
+    virtual void getAnalysisUsage(AnalysisUsage &Info) const {
     }
   };
 }
