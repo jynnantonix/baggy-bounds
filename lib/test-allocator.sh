f6f6c712253bd353d34000c73e757417c2938718
Travis Hance <tjhance7@gmail.com>
2013-12-02 22:28:26 +0000

    fixed ulimit

--- lib/test-allocator.sh
+++ lib/test-allocator.sh
@@ -1,4 +1,4 @@
-ulimit -s 1056896
+ulimit -s 1318912
 rm test.o
 gcc allocator.c baggy_init.c test.c move_stack.S  -o test.o -Wall -g
 #gcc allocator.c baggy_init.c test.c move_stack.S -O3 -o test.o -Wall -g
