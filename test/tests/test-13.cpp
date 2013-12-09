78c43b2789a15444e7e3dac0eed9651b9822ad8e
Travis Hance <tjhance7@gmail.com>
2013-12-09 21:00:24 +0000

    added some tests

---
+++ test/tests/test-13.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+void __attribute__ ((noinline)) run(char* a, char* b, char* c) {
+	for (int i = 0; i < 16; i++) {
+		a[i] = 1;
+		c[i] = 1;
+	}
+
+	for (int i = 0; i < 64; i++) {
+		b[i] = 3;
+	}
+}
+
+void __attribute__ ((noinline)) output(char* a, char* b, char* c) {
+	for (int i = 0; i < 16; i++) {
+		printf("%d,", (int) a[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < 16; i++) {
+		printf("%d,", (int) b[i]);
+	}
+	printf("\n");
+}
+
+int main() {
+	char a[16];
+	char b[31];
+	char c[16];
+
+	run(a, b, c);
+
+	output(a, b, c);
+}
