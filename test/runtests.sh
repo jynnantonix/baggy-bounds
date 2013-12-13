c277c483e452f2916533cf2088d8ef190b38286b
Travis Hance <tjhance7@gmail.com>
2013-12-13 18:18:09 +0000

    added make clean to the runtests script

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,5 +1,6 @@
 ulimit -s 1318912
 BASEDIR=`dirname $0`
+make clean > /dev/null
 make -C $BASEDIR/../pass/build/ > /dev/null
 make -C $BASEDIR/../lib/ > /dev/null
 $BASEDIR/find_and_run_tests.py "$@"
